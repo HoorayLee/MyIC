049b94c4c59584b398e7908f4238a45bf6b6cba3
HoorayLee <leehorray@gmail.com>
2014-12-07 10:02:57 +0800

    A wp app which can detect your oral problems.
    This is UI design.

---
+++ eDentist/PanoramaApp1/Questions.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PanoramaApp1
+{
+    public partial class Page2 : PhoneApplicationPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
