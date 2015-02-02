212046d5a5b58672a64ad31c54a0fd01c080e6c7
TonyPod <843678719@qq.com>
2015-02-02 11:59:56 +0800

    代码生成器
    连接数据库生成DAL层和model代码

---
+++ CodeGen/CodeGen/MainForm.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CodeGen
+{
+    public partial class MainForm : Form
+    {
+        private static readonly string connStr = ConfigurationManager.ConnectionStrings["Geek"].ConnectionString;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            tb_connStr.Text = connStr;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 点击事件：连接
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connStr = tb_connStr.Text;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "select table_name from information_schema.tables where table_type = 'base table'";
+                    DataSet dataSet = new DataSet();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.FillSchema(dataSet, SchemaType.Source);
+                    adapter.Fill(dataSet);
+
+                    DataTable table = dataSet.Tables[0];
+                    foreach (DataRow row in table.Rows)
+                    {
+                        string tableName = (string)row["table_name"];
+                        cb_tables.Items.Add(tableName);
+                    }
+
+                    cb_tables.Enabled = true;
+                    btn_gen.Enabled = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 点击事件：生成
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_gen_Click(object sender, EventArgs e)
+        {
+            if (cb_tables.SelectedIndex < 0)
+            {
+                MessageBox.Show("请选择数据库表名");
+                return;
+            }
+
+            string tableName = cb_tables.SelectedItem.ToString();
+            string nameSpace = tb_namespace.Text;
+
+            tb_model.Text = GenModels(tableName, nameSpace);
+            tb_DAL.Text = GenDAL(tableName, nameSpace);
+        }
+
+        private string GenDAL(string tableName, string nameSpace)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            string className = FirstLetterUpper(tableName) + "DAL";
+            string modelClassName = FirstLetterUpper(tableName) + "Model";
+            string objName = FirstLetterLower(tableName);
+
+            builder.Append("public class ").AppendLine(className);
+            builder.AppendLine("{");
+
+            DataTable table = SqlHelper.ExecuteDataTable("select top 0 * from [" + tableName + "]");
+
+            //1.Insert
+            builder.AppendFormat("\tpublic static void Insert({0} {1})", modelClassName, objName).AppendLine();
+            builder.AppendLine("\t{");
+            builder.Append("\t\tSqlHelper.ExecuteNonQuery(@\"insert into ").Append(modelClassName).
+                AppendLine(GetColsString(table, false));
+            builder.Append("\t\t\tvalues").Append(GetColsString(table, true)).AppendLine("\",");
+            builder.Append(GetParamsString(table, objName));
+            builder.AppendLine("\t\t);");
+            builder.AppendLine("\t}");
+            builder.AppendLine();
+
+            //2.DeleteById
+            builder.AppendLine("\tpublic static void DeleteById(long id)");
+            builder.AppendLine("\t{");
+            builder.Append("\t\tSqlHelper.ExecuteNonQuery(@\"delete from ").Append(tableName).AppendLine(" where id = @id\",");
+            builder.AppendLine("\t\t\tnew SqlParameter(\"@id\", id));");
+            builder.AppendLine("\t}");
+            builder.AppendLine();
+
+            //3.Update
+            builder.Append("\tpublic static void Update(").Append(modelClassName).Append(" ").Append(objName).
+                AppendLine(")");
+            builder.AppendLine("\t{");
+            builder.Append("\t\tSqlHelper.ExecuteNonQuery(@\"update ").Append(tableName).AppendLine(" set");
+            builder.Append(GetColsStringForSet(table));
+            builder.AppendLine("\t\t\twhere id = @id\",");
+            builder.Append(GetParamsString(table, objName));
+            builder.AppendLine("\t\t);");
+            builder.AppendLine("\t}");
+            builder.AppendLine();
+
+            //4.GetById
+            builder.Append("\tpublic static ").Append(modelClassName).AppendLine(" GetById(long id)");
+            builder.AppendLine("\t{");
+            builder.Append("\t\tDataTable table = SqlHelper.ExecuteDataTable(@\"select * from ").Append(tableName).
+                AppendLine(" where id = @id\",");
+            builder.AppendLine("\t\t\tnew SqlParameter(\"@id\", id));");
+            builder.AppendLine("\t\tif (table.Rows.Count <= 0)");
+            builder.AppendLine("\t\t{");
+            builder.AppendLine("\t\t\treturn null;");
+            builder.AppendLine("\t\t}");
+            builder.AppendLine("\t\telse if (table.Rows.Count > 1)");
+            builder.AppendLine("\t\t{");
+            builder.Append("\t\t\tthrow new Exception(\"Fatal Error: Duplicated Id in Table ").Append(tableName).
+                AppendLine("\");");
+            builder.AppendLine("\t\t}");
+            builder.AppendLine("\t\telse");
+            builder.AppendLine("\t\t{");
+            builder.AppendLine("\t\t\tDataRow row = table.Rows[0];");
+            builder.AppendLine("\t\t\treturn ToModel(row);");
+            builder.AppendLine("\t\t}");
+            builder.AppendLine("\t}");
+            builder.AppendLine();
+
+            //5.GetAll()
+            builder.Append("\tpublic static ").Append(modelClassName).AppendLine("[] GetAll()");
+            builder.AppendLine("\t{");
+            builder.Append("\t\tDataTable table = SqlHelper.ExecuteDataTable(\"select * from ").Append(tableName).
+                AppendLine("\");");
+            builder.Append("\t\t").Append(modelClassName).Append("[] ").Append(objName).Append(" = new ").
+                Append(modelClassName).AppendLine("[table.Rows.Count];");
+            builder.AppendLine("\t\tfor (int i = 0; i < table.Rows.Count; i++)");
+            builder.AppendLine("\t\t{");
+            builder.Append("\t\t\t").Append(objName).AppendLine("[i] = ToModel(table.Rows[i]);");
+            builder.AppendLine("\t\t}");
+            builder.Append("\t\treturn ").Append(objName).AppendLine(";");
+            builder.AppendLine("\t}");
+            builder.AppendLine();
+
+            //6.ToModel
+            builder.Append("\tprivate static ").Append(modelClassName).AppendLine(" ToModel(DataRow row)");
+            builder.AppendLine("\t{");
+            builder.AppendFormat("\t\t{0} {1} = new {0}();", modelClassName, objName).AppendLine();
+            foreach (DataColumn col in table.Columns)
+            {
+                builder.AppendFormat("\t\t{0}.{1} = ({2})row[\"{3}\"];", objName, FirstLetterUpper(col.ColumnName), 
+                    col.DataType.ToString(), FirstLetterLower(col.ColumnName)).AppendLine();
+            }
+
+            builder.AppendFormat("\t\treturn {0};", objName).AppendLine();
+            builder.AppendLine("\t}");
+
+            builder.AppendLine("}");
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// 生成数据模型
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="nameSpace"></param>
+        /// <returns></returns>
+        private string GenModels(string tableName, string nameSpace)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            //是否输入命名空间
+            if (string.IsNullOrWhiteSpace(nameSpace))
+            {
+                builder.Append("public class ").AppendLine(FirstLetterUpper(tableName) + "Model");
+                builder.AppendLine("{");
+
+                DataTable table = SqlHelper.ExecuteDataTable("select top 0 * from [" + tableName + "]");
+
+                foreach (DataColumn col in table.Columns)
+                {
+                    builder.Append("\tpublic ").Append(GetColumnType(col) + " ").Append(FirstLetterUpper(col.ColumnName)).AppendLine("{ get; set; }");
+                }
+
+                builder.AppendLine("}");
+                return builder.ToString(); 
+            }
+            else
+            {
+                builder.Append("namespace ").AppendLine(nameSpace);
+                builder.AppendLine("{");
+                builder.Append("\tpublic class ").AppendLine(FirstLetterUpper(tableName));
+                builder.AppendLine("\t{");
+
+                DataTable table = SqlHelper.ExecuteDataTable("select top 0 * from [" + tableName + "]");
+
+                foreach (DataColumn col in table.Columns)
+                {
+                    builder.Append("\t\tpublic ").Append(GetColumnType(col) + " ").Append(col.ColumnName).AppendLine("{ get; set; }");
+                }
+
+                builder.AppendLine("\t}");
+                builder.AppendLine("}");
+                return builder.ToString();
+            }
+        }
+
+        private string GetColumnType(DataColumn col)
+        {
+            return col.DataType + ((col.AllowDBNull && col.DataType.IsValueType) ? "?" : "");
+        }
+
+        private string FirstLetterUpper(string str)
+        {
+            return str.Substring(0, 1).ToUpper() + str.Substring(1);
+        }
+
+        private string FirstLetterLower(string str)
+        {
+            return str.Substring(0, 1).ToLower() + str.Substring(1);
+        }
+
+        /// <summary>
+        /// Insert中需要用到各列的名字，不含自增长的
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="withAt"></param>
+        /// <returns></returns>
+        private string GetColsString(DataTable table, bool withAt)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("(");
+            int i;
+            DataColumn col;
+            if (withAt)
+            {
+                for (i = 0; i < table.Columns.Count - 1; i++)
+                {
+                    col = table.Columns[i];
+                    if (!col.AutoIncrement)
+                    {
+                        builder.Append(table.Columns[i].ColumnName).Append(", "); 
+                    }
+                }                
+            }
+            else
+            {
+                for (i = 0; i < table.Columns.Count - 1; i++)
+                {
+                    col = table.Columns[i];
+                    if (!col.AutoIncrement)
+                    {
+                        builder.Append("@").Append(table.Columns[i].ColumnName).Append(", ");
+                    }
+                } 
+            }
+
+            col = table.Columns[i];
+            if (!col.AutoIncrement)
+            {
+                builder.Append(table.Columns[i].ColumnName);
+            }
+            builder.Append(")");
+
+            return builder.ToString();
+        }
+
+        private string GetColsStringForSet(DataTable table)
+        {
+            StringBuilder builder = new StringBuilder();
+            int i;
+            DataColumn col;
+            string colName;
+            for (i = 0; i < table.Columns.Count - 1; i++)
+            {
+                col = table.Columns[i];
+                colName = col.ColumnName;
+                if (!col.AutoIncrement)
+                {
+                    builder.Append("\t\t\t").Append(colName).Append(" = @").Append(colName).AppendLine(",");
+                }
+            } 
+            col = table.Columns[i];
+            colName = col.ColumnName;
+            if (!col.AutoIncrement)
+            {
+                builder.Append("\t\t\t").Append(colName).Append(" = @").Append(colName).AppendLine();
+            }
+
+            return builder.ToString();
+        }
+
+        private string GetParamsString(DataTable table, string objName)
+        {
+            StringBuilder builder = new StringBuilder();
+            int i;
+            DataColumn col;
+            for (i = 0; i < table.Columns.Count - 1; i++)
+            {
+                col = table.Columns[i];
+                if (!col.AutoIncrement)
+                {
+                    builder.Append("\t\t\tnew SqlParameter(\"@").Append(col.ColumnName).Append("\", ").
+                        Append(objName).Append(".").Append(FirstLetterUpper(col.ColumnName)).AppendLine("),");
+                }
+            }
+            col = table.Columns[i];
+            if (!col.AutoIncrement)
+            {
+                builder.Append("\t\t\tnew SqlParameter(\"@").Append(col.ColumnName).Append("\", ").
+                    Append(objName).Append(".").Append(FirstLetterUpper(col.ColumnName)).AppendLine(")");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ CodeGen/CodeGen/SqlHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace CodeGen
+{
+    class SqlHelper
+    {
+        private static readonly string connStr = ConfigurationManager.ConnectionStrings["Geek"].ConnectionString;
+
+        public static int ExecuteNonQuery(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static DataTable ExecuteDataTable(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    DataSet dataSet = new DataSet();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.FillSchema(dataSet, SchemaType.Source);
+                    adapter.Fill(dataSet);
+                    return dataSet.Tables[0]; 
+                }
+            }
+        }
+
+        public static object ExecuteScalar(string sql, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public static object FromDBValue(object value)
+        {
+            if (value == DBNull.Value)
+            {
+                return null;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public static object ToDBValue(object value)
+        {
+            if (value == null)
+            {
+                return DBNull.Value;
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+}
